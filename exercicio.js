b8be83b7ab8adf003509fc73d327e3dab7d78ea3
YagoHOC <108764796+YagoHOC@users.noreply.github.com>
2023-08-22 21:05:14 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,53 @@
+//exercicio 1
+
+//let nome 
+//let idade
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+
+//foi impresso undefined pois ainda não foi atribuído nenhum valor
+
+//nome = prompt('Qual é seu nome?')
+//idade = prompt('Quantos anos você tem?')
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+
+//os dois viraram strings, pois foi inserido um texto como conteúdo, que por padrão é uma string
+//console.log('Olá',nome,'você tem',idade,'anos de idade.')
+
+//exercicio 2
+
+//let roupaCor = prompt('Você está usando uma roupa preta hoje?')
+//let tenisTipo = prompt('Você está utilizando um chinelo hoje?')
+//let acessorio = prompt('Você está usando algum acessório hoje?')
+
+//console.log('Você está usando uma roupa preta hoje?',roupaCor)
+//console.log('Você está utilizando um chinelo hoje?',tenisTipo)
+//console.log('Você está usando algum acessório hoje?',acessorio)
+
+//exercicio 3
+
+//let a = 10
+//let b = 25
+
+//let b2 = a
+//a = b
+
+//console.log("O novo valor de a é", a) // O novo valor de a é 25
+//console.log("O novo valor de b é", b2) // O novo valor de b é 10
+
+//Desafio
+
+//1. O primeiro número somado ao segundo número resulta em: x.
+//2. O primeiro número multiplicado pelo segundo número resulta em: y.
+
+//let numeroA = Number(prompt('Escolha o número A'))
+//let numeroB = Number(prompt('Escolha o número B'))
+
+//let x = numeroA + numeroB
+//let y = numeroA * numeroB
+
+//console.log(x)
+//console.log(y)
